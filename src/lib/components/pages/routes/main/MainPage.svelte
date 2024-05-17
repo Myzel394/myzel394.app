952681fb485d9ea3eabd9cc73feabcec39b69fc3
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-05-17 23:15:25 +0200

    feat: Add MainPage logo

---
+++ src/lib/components/pages/routes/main/MainPage.svelte
@@ -0,0 +1,50 @@
+<main>
+    <section>
+        <div class="text">
+            <img src={appIcon} alt="NumberHub" />
+            <h1>NumberHub</h1>
+            <p>
+                The only calculator you'll ever need.
+            </p>
+        </div>
+    </section>
+</main>
+
+<script>
+import appIcon from "$lib/assets/app-icon.svg";
+</script>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    section {
+        padding: 8em 2em;
+    }
+
+    h1 {
+        font-size: 3rem;
+        font-size: 10cqw;
+        font-family: "Montserrat", Montserrat, "Roboto", Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;
+        font-weight: 800;
+    }
+
+    img {
+        width: 10em;
+        height: 10em;
+        border-radius: 25%;
+        box-shadow: 0 .5em 15em .2em rgba(var(--on-background-null-secondary-color), 0.3);
+    }
+
+    .text {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        row-gap: 2em;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,30 @@
+<MainPage />
+
+<script>
+	import MainPage from "$lib/components/pages/routes/main/MainPage.svelte";
+</script>
 
 <style>
 :global(html) {
-    --background-null-color: #131613;
-    --background-color: #191C19;
-    --surface-color: #1D211D;
-    --surface-tint-color: #C0CDBE;
+    --background-null-color: 19, 22, 19;
+    --background-color: 25, 28, 25;
+    --surface-color: 29, 33, 29;
+    --surface-tint-color: 192, 205, 190;
 
-    --on-background-null-primary-color: #FFF;
-    --on-background-null-secondary-color: #79B4AB;
+    --on-background-null-primary-color: 255, 255, 255;
+    --on-background-null-secondary-color: 121, 180, 171;
 
-    --on-background-primary-color: #DAF0DA;
-    --on-background-secondary-color: #79B4AB;
+    --on-background-primary-color: 218, 240, 218;
+    --on-background-secondary-color: 121, 180, 171;
 
-    --on-surface-primary-color: #B4D6B4;
-    --on-surface-secondary-color: #79B4AB;
+    --on-surface-primary-color: 180, 214, 180;
+    --on-surface-secondary-color: 121, 180, 171;
 
-    --on-surface-tint-primary-color: #DDEBDB;
-    --on-surface-tint-secondary-color: #C0CDBE;
+    --on-surface-tint-primary-color: 221, 235, 219;
+    --on-surface-tint-secondary-color: 192, 205, 190;
+
+    background: rgb(var(--background-color));
+    color: rgb(var(--on-background-primary-color));
+    font-family: 'Roboto', Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;
 }
 </style>
