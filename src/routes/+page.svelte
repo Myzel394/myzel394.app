24da3989e238ff13de39cc871a86d7e7e2d20fce
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-05-17 22:12:18 +0200

    feat: Add colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+:global(html) {
+    --background-null-color: #131613;
+    --background-color: #191C19;
+    --surface-color: #1D211D;
+    --surface-tint-color: #C0CDBE;
+
+    --on-background-null-primary-color: #FFF;
+    --on-background-null-secondary-color: #79B4AB;
+
+    --on-background-primary-color: #DAF0DA;
+    --on-background-secondary-color: #79B4AB;
+
+    --on-surface-primary-color: #B4D6B4;
+    --on-surface-secondary-color: #79B4AB;
+
+    --on-surface-tint-primary-color: #DDEBDB;
+    --on-surface-tint-secondary-color: #C0CDBE;
+}
+</style>
