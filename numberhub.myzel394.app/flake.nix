ac8cab2d37e4de0e3bd96cc36a3a7324b9517499
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-05-23 17:54:49 +0200

    feat: Build nix for all systems

--- numberhub.myzel394.app/flake.nix
+++ numberhub.myzel394.app/flake.nix
@@ -1,40 +1,32 @@
 {
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        systems.url = "github:nix-systems/default";
-        gitignore = {
-            url = "github:hercules-ci/gitignore.nix";
-            inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        packages.default = pkgs.buildNpmPackage {
+          pname = "numberhub.myzel394.app";
+          version = "0.0.1";
+
+          src = self;
+          npmDepsHash = "sha256-RZc7YMCvS5ds/gSDMJr7IPp/UriiF7w1C2NO/hAJ0w4=";
+          npmBuildScript = "build";
+
+          # The prepack script runs the build script, which we'd rather do in the build phase.
+          npmPackFlags = [ "--ignore-scripts" ];
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir $out
+            cp -r build $out/build
+
+            runHook postInstall
+          '';
         };
-    };
-    outputs = { self, systems, nixpkgs, gitignore }:
-        let
-            eachSystem = packages:
-                nixpkgs.lib.genAttrs (import systems) (
-                    system:
-                    packages nixpkgs.legacyPackages.${system}
-                );
-        in
-        {
-            packages.${system}.default = pkgs.buildNpmPackage {
-                inherit nodejs;
-                name = "numberhub.myzel394.app";
-
-                src = gitignore.lib.gitignoreSource ./.;
-                npmDepsHash = "sha256-RZc7YMCvS5ds/gSDMJr7IPp/UriiF7w1C2NO/hAJ0w4=";
-                npmBuildScript = "build";
-
-                # The prepack script runs the build script, which we'd rather do in the build phase.
-                npmPackFlags = [ "--ignore-scripts" ];
-
-                installPhase = ''
-                    runHook preInstall
-
-                    mkdir $out
-                    cp -r build $out/build
-
-                    runHook postInstall
-                    '';
-            };
-        };
+      });
 }
+
