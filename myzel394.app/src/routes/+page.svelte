f0c46b398dc1ac348dbfb715bfc7c44f8138ecd4
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-10-20 14:57:38 +0200

    feat(myzel394.app): Add myzel394.app

---
+++ myzel394.app/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<div>
+    <h1>Myzel394</h1>
+    <p>This site is under construction</p>
+</div>
+
+<style>
+:global(html, body) {
+    width: 100%;
+    min-height: 100vh;
+    height: 100%;
+    background: #232323;
+}
+
+div {
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-family: Montserrat, Helvetica Neue, Google Sans, Arial, sans-serif;
+}
+
+h1 {
+    font-size: 5rem;
+    color: #fff;
+}
+
+p {
+    color: #c0c0c0;
+}
+</style>
+
